cd93fb410ec8ada894cf14d9a32197b86f6943da
Alejandro Tejada <alectejada@gmail.com>
2017-09-04 13:54:24 -0400

    login component initial markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { RouterModule, Route } from "@angular/router";
 
 import { AppComponent }  from './app.component';
 import { HomeComponent }  from './home.component';
+import { LoginComponent }  from './login.component';
 import AppRoutes from './app.routes';
 
 @NgModule({
   imports:      [ BrowserModule, RouterModule.forRoot(AppRoutes)  ],
-  declarations: [ AppComponent, HomeComponent ],
+  declarations: [ AppComponent, HomeComponent, LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Route } from "@angular/router";
 import {
   HomeComponent,
-} from "./home.component";
+} from './home.component';
+
+import {
+  LoginComponent,
+} from './login.component';
 
 const routes: Route[] = [
-    { path: "home", component: HomeComponent },
-    { path: "", redirectTo: "home", pathMatch: "full"}
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 export default routes;
