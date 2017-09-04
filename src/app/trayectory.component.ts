0d50d11e405eb174c77339147c052941a301b354
Jose Andrade <jose1310@gmail.com>
2017-09-04 16:03:18 -0400

    jandrade trayectory

--- src/app/trayectory.component.ts
+++ src/app/trayectory.component.ts
@@ -8,13 +8,43 @@ import { Component } from '@angular/core';
 
       <!-- Portfolio Item Row -->
       <div class="row">
+	    <div class="col-md-4 col-sm-6 mb-4">
+          <div class="lms-course-box lms-course">
+            <a href="#">
+              <div class="lms-course-box lms-course-title">
+                <img class=".img-responsive" style="max-height:100px" src="./images/frontend.png" alt="Front End Developer">
+                <div class="lms-course-description">
+                  Front End Developer
+                </div>
+              </div>
+              <div class="lms-course-author">
+                Pedro Perez
+            </div>
+            </a>
+          </div>
+        </div>
+		<div class="col-md-4 col-sm-6 mb-4">
+          <div class="lms-course-box lms-course">
+            <a href="#">
+              <div class="lms-course-box lms-course-title">
+                <img class=".img-responsive" style="max-height:100px" src="./images/backend.png" alt="Back End Developer">
+                <div class="lms-course-description">
+                  Back End Developer
+                </div>
+              </div>
+              <div class="lms-course-author">
+                Pedro Perez
+            </div>
+            </a>
+          </div>
+        </div>
+		<!--
 		<div class="col-md-4"><img class=".img-responsive" style="max-width:400px" src="./images/frontend.png" alt="Front End Developer"></div>
 		<div class="col-md-4"><img class=".img-responsive" style="max-width:400px" src="./images/backend.png" alt="Back End Developer"></div>		
 		<div class="col-md-4"><img class=".img-responsive" style="max-width:400px" src="./images/Quality-Assurance.jpg" alt=" QA Developer"></div>
+		-->
       </div>
-	        <div class="row">
-		<div class="col-md-4">SDET</div>
-      </div>
+
       <!-- /.row -->
   `
 })
